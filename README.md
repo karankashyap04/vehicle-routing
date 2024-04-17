c3143f5f8745f30389c71b9b3e7cfb6ed3eaf568
Karan Kashyap <karankashyap@gmail.com>
2024-04-17 00:37:35 -0400

    chore: fix formatting; add comments to README

--- README.md
+++ README.md
@@ -4,3 +4,45 @@
 
 - **Multithreading**: we can use multithreading (with a thread pool) for calculating scores for different neighbors in a neighborhood to speed things up (while identifying where to move next in the solution space)
 - **Constructing initial solution**: maybe we could use IBM's CP solver (or maybe CPLEX) to construct the initial solution -- I think we could formulate the constraints like those for a binpacking problem (vehicles correspond to bins). CP might also have an inbuilt binpacking constraint, which could be helpful.
+
+
+## Thoughts About IP Model
+
+**NOTE** We eventually discarded our attempt to make an IP model; we realized this wasn't helpful/straightforward -- using Google's
+OR tools VRP solver to get rough approximations of optimal results was a better option.
+
+Attempt 1:
+- Vehicles numbered 1 to numVehicles (0 in matrix represents no vehicle assigned)
+- `numCustomers x numCustomers` matrix of decision vars -- to represent the edges in the graph. The value stored represents which vehicle traversed the edge
+- `numTrucks` array of decision vars -- represents trucks used in solution
+- Constraints
+  - Paths start at the warehouse -- constrain using `vehicleUsed` decision variable
+  - Paths end at the warehouse -- might not need to include that it actually returns in constraints; just add dist to warehouse in optimization function
+  - Every node should have at least one incoming edge (sum of column is greater than or equal to 1)
+    - We expect in minimization cplex will not assign multiple vehicles to visit the same customer
+
+Attempt 2:
+- Decision vars
+  - `numCustomers x (numCustomers - 1)` matrix of values. Each value represents an edge in the graph. The value stored represents the number of times the edge is traversed
+    - `matrix[i][j]` is whether a vehicle went from customer `i` to customer `j`
+    - Each value is 0 or 1 (except for vars on the diagonal -- these values _must_ be 0)
+    - #columns is `numCustomers - 1`: this is because we will not make paths go back to the warehouse in the model. We will just add back that cost 
+- Constraints
+  - All customers must have one incoming and one outgoing edge -- entries not in the first row or column must have sum of column and sum of row equal to 1
+  - The sum of the 0th row must be the same as the sum of the 0th column (each vehicle must begin and end at the warehouse)
+
+
+## Next Steps 
+
+Local Search Attempt 1:
+- Define CP model to get initial candidate solution
+- Define moving mechanism to construct neighborhood
+  - Move one customer to same index in all the paths
+  - Move one customer to all indices of a single path
+- Filter our infeasible neighbors
+- Take best improvement from feasible neighbors / random choice with some probability 
+
+TODO:
+- Set up CP solver
+- Solution class (to store solution)
+- Solution verification class (feasibility checker)
--- src/solver/ls/VRPInstance.java
+++ src/solver/ls/VRPInstance.java
@@ -5,48 +5,69 @@ import java.io.FileNotFoundException;
 
 import java.util.Scanner;
 
-public class VRPInstance
-{
-  // VRP Input Parameters
-  int numCustomers;        		// the number of customers	   
-  int numVehicles;           	// the number of vehicles
-  int vehicleCapacity;			// the capacity of the vehicles
-  int[] demandOfCustomer;		// the demand of each customer
-  double[] xCoordOfCustomer;	// the x coordinate of each customer
-  double[] yCoordOfCustomer;	// the y coordinate of each customer
-  
-  public VRPInstance(String fileName)
-  {
-    Scanner read = null;
-    try
-    {
-      read = new Scanner(new File(fileName));
-    } catch (FileNotFoundException e)
-    {
-      System.out.println("Error: in VRPInstance() " + fileName + "\n" + e.getMessage());
-      System.exit(-1);
+public class VRPInstance {
+    // VRP Input Parameters
+    int numCustomers;                // the number of customers
+    int numVehicles;            // the number of vehicles
+    int vehicleCapacity;            // the capacity of the vehicles
+    int[] demandOfCustomer;        // the demand of each customer
+    double[] xCoordOfCustomer;    // the x coordinate of each customer
+    double[] yCoordOfCustomer;    // the y coordinate of each customer
+    double[][] distance;        // distances between all customers (including warehouse)
+
+
+    public VRPInstance(String fileName) {
+        Scanner read = null;
+        try {
+            read = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: in VRPInstance() " + fileName + "\n" + e.getMessage());
+            System.exit(-1);
+        }
+
+        numCustomers = read.nextInt();
+        numVehicles = read.nextInt();
+        vehicleCapacity = read.nextInt();
+
+        System.out.println("Number of customers: " + numCustomers);
+        System.out.println("Number of vehicles: " + numVehicles);
+        System.out.println("Vehicle capacity: " + vehicleCapacity);
+
+        demandOfCustomer = new int[numCustomers];
+        xCoordOfCustomer = new double[numCustomers];
+        yCoordOfCustomer = new double[numCustomers];
+
+        for (int i = 0; i < numCustomers; i++) {
+            demandOfCustomer[i] = read.nextInt();
+            xCoordOfCustomer[i] = read.nextDouble();
+            yCoordOfCustomer[i] = read.nextDouble();
+        }
+
+        for (int i = 0; i < numCustomers; i++)
+            System.out.println(demandOfCustomer[i] + " " + xCoordOfCustomer[i] + " " + yCoordOfCustomer[i]);
+
+        this.generateDistanceMatrix();
     }
 
-    numCustomers = read.nextInt(); 
-    numVehicles = read.nextInt();
-    vehicleCapacity = read.nextInt();
-    
-    System.out.println("Number of customers: " + numCustomers);
-    System.out.println("Number of vehicles: " + numVehicles);
-    System.out.println("Vehicle capacity: " + vehicleCapacity);
-      
-    demandOfCustomer = new int[numCustomers];
-	xCoordOfCustomer = new double[numCustomers];
-	yCoordOfCustomer = new double[numCustomers];
-	
-    for (int i = 0; i < numCustomers; i++)
-	{
-		demandOfCustomer[i] = read.nextInt();
-		xCoordOfCustomer[i] = read.nextDouble();
-		yCoordOfCustomer[i] = read.nextDouble();
-	}
-	
-	for (int i = 0; i < numCustomers; i++)
-		System.out.println(demandOfCustomer[i] + " " + xCoordOfCustomer[i] + " " + yCoordOfCustomer[i]);
-  }
+    private double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+    private void generateDistanceMatrix() {
+        this.distance = new double[numCustomers][numCustomers];
+        for (int i = 0; i < numCustomers; i++) {
+            for (int j = 0; j < numCustomers; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                    continue;
+                }
+                double x1 = xCoordOfCustomer[i];
+                double y1 = yCoordOfCustomer[i];
+                double x2 = xCoordOfCustomer[j];
+                double y2 = yCoordOfCustomer[j];
+                double dist = this.distance(x1, y1, x2, y2);
+                this.distance[i][j] = dist;
+            }
+        }
+    }
 }
